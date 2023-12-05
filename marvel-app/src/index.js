7f8f5b8025e84261ee99f7fac4ed9a163ec5c2a5
andreysulymka <andreysulymka5@gmail.com>
2023-12-05 17:53:35 +0200

    add project with class components

---
+++ marvel-app/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/app/App";
+
+
+import "./style/style.scss";
+
+
+
+const root = document.getElementById("root");
+const rootElement = createRoot(root);
+rootElement.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
