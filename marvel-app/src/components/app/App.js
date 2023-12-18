579115829f97592482d427090fb5d24ce33cf700
andreysulymka <andreysulymka5@gmail.com>
2023-12-18 18:50:24 +0200

    change components App and charList from class components in fuctional components

--- marvel-app/src/components/app/App.js
+++ marvel-app/src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
@@ -8,19 +8,15 @@ import ErrorBoundary from "../errorBoundary/ErrorBoudary";
 
 import decoration from "../../resources/img/vision.png";
 
-class App extends Component {
-  state = {
-    selectedChar: null,
+const App = () => {
+
+  const [selectedChar, setChar] = useState(null)
+ 
+  const onCharSelected = (id) => {
+   setChar(id)
   };
 
-  onCharSelected = (id) => {
-    this.setState({
-      selectedChar: id,
-    });
-  };
-
-  render() {
-    return (
+     return (
       <div className="app">
         <AppHeader />
         <main>
@@ -29,10 +25,10 @@ class App extends Component {
           </ErrorBoundary>
           <div className="char__content">
             <ErrorBoundary>
-              <CharList onCharSelected={this.onCharSelected} />
+              <CharList onCharSelected={onCharSelected} />
             </ErrorBoundary>
             <ErrorBoundary>
-              <CharInfo charId={this.state.selectedChar} />
+              <CharInfo charId={selectedChar} />
             </ErrorBoundary>
           </div>
           <img className="bg-decoration" src={decoration} alt="vision" />
@@ -40,6 +36,6 @@ class App extends Component {
       </div>
     );
   }
-}
+
 
 export default App;
