2c4f81d5928a933c0b22774424e5749c6dbacdc8
andreysulymka <andreysulymka5@gmail.com>
2024-01-02 22:30:20 +0200

    add all dynamic routes

--- marvel-app/src/components/app/App.js
+++ marvel-app/src/components/app/App.js
@@ -1,23 +1,22 @@
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 
 import MainPage from "../pages/MainPage";
 import ComicsPage from "../pages/ComicsPage";
-import Page404 from '../pages/404';
+import Page404 from "../pages/404";
+import SingleComicPage from "../pages/SingleComicPage";
 
 const App = () => {
-  
-
   return (
     <Router>
       <div className="app">
         <AppHeader />
         <main>
           <Routes>
-            <Route path="/" element={<MainPage />}/>
+            <Route path="/" element={<MainPage />} />
             <Route path="/comics" element={<ComicsPage />} />
-            <Route path="*" element={<Page404/> }/>
+            <Route path="/comics/:comicId" element={<SingleComicPage />} />
+            <Route path="*" element={<Page404 />} />
           </Routes>
         </main>
       </div>
