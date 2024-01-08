9e6cff12777bb7e1f2cafdd610bbb98c1a619926
andreysulymka <andreysulymka5@gmail.com>
2024-01-08 20:07:46 +0200

    add form and change layout

--- marvel-app/src/components/app/App.js
+++ marvel-app/src/components/app/App.js
@@ -1,13 +1,19 @@
-import {lazy, Suspense} from 'react'
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
-import Spinner from '../Spinner/Spinner';
+import Spinner from "../Spinner/Spinner";
 
-const Page404 = lazy(() => import('../pages/404'))
+const Page404 = lazy(() => import("../pages/404"));
 const MainPage = lazy(() => import("../pages/MainPage"));
 const ComicsPage = lazy(() => import("../pages/ComicsPage"));
-const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
+const SinglePage = lazy(() => import("../pages/SinglePage"));
+const SingleComicLayout = lazy(() =>
+  import("../pages/singleComicLayout/SingleComicLayout")
+);
+const SingleCharacterLayout = lazy(() =>
+  import("../pages/singleCharacterLayout/SingleCharacterLayout")
+);
 
 const App = () => {
   return (
@@ -15,11 +21,19 @@ const App = () => {
       <div className="app">
         <AppHeader />
         <main>
-          <Suspense fallback={<Spinner/>}>
+          <Suspense fallback={<Spinner />}>
             <Routes>
               <Route path="/" element={<MainPage />} />
               <Route path="/comics" element={<ComicsPage />} />
-              <Route path="/comics/:comicId" element={<SingleComicPage />} />
+              <Route path="/comics/:id">
+                <SinglePage Component={SingleComicLayout} dataType="comic" />
+              </Route>
+              <Route path="/characters/:id">
+                <SinglePage
+                  Component={SingleCharacterLayout}
+                  dataType="character"
+                />
+              </Route>
               <Route path="*" element={<Page404 />} />
             </Routes>
           </Suspense>
