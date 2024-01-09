16f00bac7f0d26c84752c3f17da82734e9443bcd
andreysulymka <andreysulymka5@gmail.com>
2024-01-09 16:41:58 +0200

    add changes for right rendering routes SinglePage

--- marvel-app/src/components/app/App.js
+++ marvel-app/src/components/app/App.js
@@ -25,15 +25,21 @@ const App = () => {
             <Routes>
               <Route path="/" element={<MainPage />} />
               <Route path="/comics" element={<ComicsPage />} />
-              <Route path="/comics/:id">
-                <SinglePage Component={SingleComicLayout} dataType="comic" />
-              </Route>
-              <Route path="/characters/:id">
-                <SinglePage
-                  Component={SingleCharacterLayout}
-                  dataType="character"
-                />
-              </Route>
+              <Route
+                path="/comics/:id"
+                element={
+                  <SinglePage Component={SingleComicLayout} dataType="comic" />
+                }
+              />
+              <Route
+                path="/characters/:id"
+                element={
+                  <SinglePage
+                    Component={SingleCharacterLayout}
+                    dataType="character"
+                  />
+                }
+              />
               <Route path="*" element={<Page404 />} />
             </Routes>
           </Suspense>
